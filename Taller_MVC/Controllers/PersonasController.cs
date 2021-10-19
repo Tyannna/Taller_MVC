ce737df0b914fbc2c220b93f9d249486e6c078d9
Tatiana Florez <88855616+Tyannna@users.noreply.github.com>
2021-10-18 23:02:47 -0500

    Agregar archivos de proyecto.

---
+++ Taller_MVC/Controllers/PersonasController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Taller_MVC.Models;
+
+namespace Taller_MVC.Controllers
+{
+    public class PersonasController : Controller
+    {
+        private mvccrudEntities db = new mvccrudEntities();
+
+        // GET: Personas
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.personas.ToListAsync());
+        }
+
+        // GET: Personas/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            persona persona = await db.personas.FindAsync(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // GET: Personas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Personas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "id,nombres,apellidos,idtipodocumento,documento,lugarresidencia,fechanacimiento,email,telefono,usuario,password")] persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.personas.Add(persona);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(persona);
+        }
+
+        // GET: Personas/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            persona persona = await db.personas.FindAsync(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // POST: Personas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "id,nombres,apellidos,idtipodocumento,documento,lugarresidencia,fechanacimiento,email,telefono,usuario,password")] persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(persona).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(persona);
+        }
+
+        // GET: Personas/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            persona persona = await db.personas.FindAsync(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // POST: Personas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            persona persona = await db.personas.FindAsync(id);
+            db.personas.Remove(persona);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
